c01cf145dab890039098c425dfd22356922b2bbd
Jesse <zhaojian01@baidu.com>
2014-03-11 16:21:36 +0800

    fix bug

--- class/DB/Category.class.php
+++ class/DB/Category.class.php
@@ -1,4 +1,14 @@
 <?php
 class DB_Category extends DB_Model{
 	public $tableName = 'category';
+	
+	public function create($condition,$duplicateCondition = null){
+		$condition['create_time'] = $condition['create_time'] ? $condition['create_time'] : time();
+		$condition['update_time'] = $condition['update_time'] ? $condition['update_time'] : time();
+		if($duplicateCondition){
+			$duplicateCondition['update_time'] = $duplicateCondition['update_time'] ? $duplicateCondition['update_time'] : time();
+		}
+		return parent::create($condition,$duplicateCondition);
+	}
+	
 }
