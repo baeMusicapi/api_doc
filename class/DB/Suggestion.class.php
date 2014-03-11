5ad34cdbf476fe694e6be69cc104c0c13e1761d1
Jesse <zhaojian01@baidu.com>
2014-03-11 16:23:58 +0800

    fix bug

--- class/DB/Suggestion.class.php
+++ class/DB/Suggestion.class.php
@@ -1,4 +1,13 @@
 <?php
 class DB_Suggestion extends DB_Model{
 	public $tableName = 'suggestion';
+	
+	public function create($condition,$duplicateCondition = null){
+		$condition['create_time'] = $condition['create_time'] ? $condition['create_time'] : time();
+		$condition['update_time'] = $condition['update_time'] ? $condition['update_time'] : time();
+		if($duplicateCondition){
+			$duplicateCondition['update_time'] = $duplicateCondition['update_time'] ? $duplicateCondition['update_time'] : time();
+		}
+		return parent::create($condition,$duplicateCondition);
+	}
 }
